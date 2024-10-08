51e299da4116b1ddeeb1c4c882301183a6a379f3
AlbertZhaoCA <zhaoq@kean.edu>
2024-10-09 02:20:34 +0800

    chore: Update dependency - Configure webpack for js packaging - Use monaco-editor and socket.io to provide a live editor

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = {
+    entry:{
+        'live-editor': '/client/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean:true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new MonacoWebpackPlugin({
+            languages: ['html', 'javascript', 'css', 'json']
+        })
+    ],
+    mode: 'development',
+};
