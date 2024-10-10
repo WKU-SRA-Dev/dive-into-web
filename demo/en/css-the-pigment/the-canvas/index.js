a7f7e690d84f3faa766efa3571f38aeb9a28b879
AlbertZhaoCA <zhaoq@kean.edu>
2024-10-10 21:29:35 +0800

    content: Update first lab demo

---
+++ demo/en/css-the-pigment/the-canvas/index.js
@@ -0,0 +1,28 @@
+// WHEN YOU TRY TO WRITE YOUR OWN LAB CODE, DO NOT DELETE THE THROTTLE FUNCTION
+
+export const throttle = (cb, delay = 1000) => {
+    let shouldWait = false;
+    let waitingArgs;
+    const timeoutFunc = () => {
+      if (waitingArgs == null) {
+        shouldWait = false;
+      } else {
+        cb(...waitingArgs);
+        waitingArgs = null;
+        setTimeout(timeoutFunc, delay);
+      }
+    };
+  
+    return (...args) => {
+      if (shouldWait) {
+        waitingArgs = args;
+        return;
+      }
+  
+      cb(...args);
+      shouldWait = true;
+  
+      setTimeout(timeoutFunc, delay);
+    };
+  };
+  
