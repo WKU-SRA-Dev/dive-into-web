b64640fc1286978090a2cdeb25438317a8aac4a1
AlbertZhaoCA <zhaoq@kean.edu>
2024-10-13 23:13:52 +0800

    chore: Routine Update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean:true
+        clean:true,
+        publicPath: '/dist/'
     },
     module: {
         rules: [
